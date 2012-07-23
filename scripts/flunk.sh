ecaebc11dc7fcb7401e3f0953c34ed9c679e084e
pair <hbmlos-linux-dev4@zynx.com>
2012-07-23 14:32:09 -0700

    Kyle - updating flunk.sh to reflect changes allowing either flunk or plain xml to be used when creating modules

--- scripts/flunk.sh
+++ scripts/flunk.sh
@@ -4,8 +4,8 @@ SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 show_usage() {
 echo ${0##/*}" Usage:"
 echo "  Generate and Commission a Netkernel Module using the flunk DSL "
-echo "${0##/} -s MODULE_DEFINITION_FILE [-f MODULE_FOLDERS_DIRECTORY] [-m MODULES_DIRECTORY]"
-echo "[-n NETKERNEL_ROOT_DIRECTORY]"
+echo "${0##/} -s MODULE_SOURCE_DIRECTORY [-f MODULE_FOLDERS_DIRECTORY] [-m MODULES_DIRECTORY]"
+echo "[-n NETKERNEL_ROOT_DIRECTORY] [-t (xml/groovy)]"
 exit
 }
 
@@ -23,36 +23,49 @@ if [[ $ARGC -lt $MINARGS ]] ; then
  show_usage
 fi
 
+MODULE_SOURCE_DIRECTORY=""
 MODULE_FILE=""
 MODULE_FOLDERS_DIRECTORY=""
 NETKERNEL_ROOT_DIRECTORY=""
-
+MODULE_TYPE=""
 FLUNK_PATH=$SCRIPT_HOME
 MODULES_DIR=$SCRIPT_HOME
 
-while getopts s:f:m:n: opt 
+while getopts s:f:m:n:t: opt 
 do 
 	case "$opt" in
-		s) MODULE_FILE=$OPTARG;;
+		s) MODULE_SOURCE_DIRECTORY=$OPTARG;;
 		f) MODULE_FOLDERS_DIRECTORY=$OPTARG;;
 		m) MODULES_DIR=$OPTARG;;
 		n) NETKERNEL_ROOT_DIRECTORY=$OPTARG;;
+		t) MODULE_TYPE=$OPTARG;;
 		[?]) show_usage;;
 	esac
 done
 
-if [[ -z $MODULE_FILE ]]; then
-	echo "No module.groovy file given, exiting"
+echo "Generating module XML"
+
+MODULE_TYPE=$(echo $MODULE_TYPE | tr -d ' ')
+
+MODULE_FILE=$(echo $MODULE_SOURCE_DIRECTORY/module.$MODULE_TYPE | tr -d ' ')
+
+
+if [[ ! -e $MODULE_FILE ]]; then
+	echo "File $MODULE_FILE doesn't exist, exiting"
 	exit
 fi
 
-echo "Generating module XML"
-echo `pwd`
-java -jar $FLUNK_PATH/flunk.jar $MODULE_FILE > module_temp
+if [[ $MODULE_TYPE = "xml" ]]; then
+	cat $MODULE_FILE > module_temp
+elif [[ $MODULE_TYPE = "groovy" ]]; then
+	java -jar $FLUNK_PATH/flunk.jar $MODULE_FILE > module_temp
+else
+	echo "unknown file type $MODULE_TYPE provided, exiting"
+	exit
+fi
 
 MODULE_NAME=$(grep -m 1 "<uri>.*</uri>" module_temp | sed 's/.*<uri>//' | sed 's/<\/uri>.*//' | sed 's/:/./g' | tr -d ' ')
 
-MODULE_FILE=$(echo $MODULE_FILE | tr -d ' ')
 MODULES_DIR=$(echo $MODULES_DIR | tr -d ' ')
 MODULE_FOLDERS_DIRECTORY=$(echo $MODULE_FOLDERS_DIRECTORY | tr -d ' ')
 NETKERNEL_ROOT_DIRECTORY=$(echo $NETKERNEL_ROOT_DIRECTORY | tr -d ' ')
@@ -80,9 +93,6 @@ if [[ -n $NETKERNEL_ROOT_DIRECTORY ]]; then
 	echo "${NETKERNEL_ROOT_DIRECTORY}/etc/modules.d/${MODULE_NAME}.xml"
 
 	DIFF_RESULT=$(diff commission_temp ${NETKERNEL_ROOT_DIRECTORY}/etc/modules.d/${MODULE_NAME}.xml)
-	echo $DIFF_RESULT
-
-
 
 	if [[ ! -e ${NETKERNEL_ROOT_DIRECTORY}/etc/modules.d/${MODULE_NAME}.xml || -n $DIFF_RESULT ]]; then
 		echo "Adding ${MODULE_NAME}.xml to ${NETKERNEL_ROOT_DIRECTORY}/etc/modules.d"
